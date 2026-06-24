74eafc4133da34f07d75cb2e9690ad0db31b2ca1
Alexandr Lelikov <a.lelikov@emergingtravel.com>
2026-06-24 23:51:40 +0300

    Add logging to Telegram webhook setup and bot startup process
    - Add info-level logs to `_set_webhook` for easier debugging of webhook configuration
    - Log bot startup and first-run checks in the `start` method

--- app/controllers/telegram.py
+++ app/controllers/telegram.py
@@ -17,6 +17,7 @@ class TelegramController:
     async def _set_webhook(self, base_webhook_url: str) -> None:
         async def check_webhook() -> WebhookInfo | None:
             try:
+                logger.info("Checking webhook info")
                 return await self.bot.get_webhook_info(request_timeout=60)
             except Exception:
                 logger.exception("Can't get webhook info")
@@ -26,11 +27,13 @@ class TelegramController:
         if self.settings.debug:
             logger.debug(f"Current bot info: {current_webhook_info}")
         try:
+            logger.info("Start setting webhook")
             await self.bot.set_webhook(
                 f"{base_webhook_url}{self.settings.webhook_path}",
                 secret_token=self.settings.telegram_my_token,
                 drop_pending_updates=current_webhook_info.pending_update_count > 0,
                 max_connections=40 if self.settings.debug else 100,
+                request_timeout=60,
             )
             if self.settings.debug:
                 logger.debug(f"Updated bot info: {await check_webhook()}")
@@ -47,12 +50,15 @@ class TelegramController:
             logger.exception("Can't set commands")
 
     async def start(self) -> None:
+        logger.info("Starting bot")
         fr = True
         if self.settings.is_check_first_run:
+            logger.info("Checking first run")
             fr = await is_first_run(self.settings.redis_url)
             if self.settings.debug:
                 logger.debug(f"First run: {fr}")
 
         if fr:
+            logger.info("Setting webhook")
             await self._set_webhook(self.settings.base_webhook_url)
             await self._set_bot_commands_menu()
